2880b32005060c36b83af7f288d1d8bcf59ceae5
Luke Morton <lukemorton.dev@gmail.com>
2018-05-27 16:06:41 +0100

    Improve logging on deploy

--- internal/deploy/executable.go
+++ internal/deploy/executable.go
@@ -1,6 +1,7 @@
 package deploy
 
 import (
+	"io"
 	"fmt"
 	"os/exec"
 )
@@ -15,21 +16,15 @@ func ensureExecutableInstalled(name string) error {
 	return nil
 }
 
-func runExecutable(executable string, args ...string) error {
-	out, err := exec.Command(executable, args...).CombinedOutput()
-	if err != nil {
-		fmt.Printf("%s, %s", executable, args)
-		return fmt.Errorf("%s", out)
-	}
-
-	return nil
+func runExecutable(writer *io.PipeWriter, executable string, args ...string) error {
+	cmd := exec.Command(executable, args...)
+	cmd.Stdout = writer
+	cmd.Stderr = writer
+	return cmd.Run()
 }
 
-func runExecutableAndReturnOutput(executable string, args ...string) ([]byte, error) {
-	out, err := exec.Command(executable, args...).CombinedOutput()
-	if err != nil {
-		return out, fmt.Errorf("%s", out)
-	}
-
-	return out, nil
+func runExecutableAndReturnOutput(writer *io.PipeWriter, executable string, args ...string) ([]byte, error) {
+	cmd := exec.Command(executable, args...)
+	cmd.Stderr = writer
+	return cmd.Output()
 }
